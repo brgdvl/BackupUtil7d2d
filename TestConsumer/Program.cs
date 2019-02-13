478f2a489827486a51451d58a63b8eb26c628975
dvlp brg <brgdvl@pm.me>
2019-02-13 17:10:27 +0100

    misted commit

---
+++ TestConsumer/Program.cs
@@ -0,0 +1,11 @@
+﻿using BackupUtil7d2d;
+namespace TestConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BackupUtil7d2d.SFTP.EstablishConnection();
+        }
+    }
+}
